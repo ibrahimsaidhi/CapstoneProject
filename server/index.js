b0cdc6a2dbfcbf203753adfda9041ebe214a4b12
Ibrahim Said <ibrahimsaid3@cmail.carleton.ca>
2023-11-25 16:04:23 -0500

    Set up database connection and fixed server connection

--- server/index.js
+++ server/index.js
@@ -1,12 +1,34 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
+const mysql = require("mysql2");
+  
+let db_con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: '',
+    database: ''
+});
+  
+db_con.connect((err) => {
+    if (err) {
+      console.log("Database Connection Failed !!!", err);
+    } else {
+      console.log("connected to Database");
+    }
+});
+  
+module.exports = db_con;
 
 const app = express();
 require("dotenv").config();
 
-app.use(cors);
-app.use(express.json());
+// app.use(cors);
+// app.use(express.json());
 
-const server = app.listen(process.env.PORT, () => {
+app.get("/", (req, res) => {
+    res.json("Hello")
+});
+
+app.listen(process.env.PORT, () => {
     console.log(`Server started on Port ${process.env.PORT}`);
 });
