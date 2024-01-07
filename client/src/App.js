4dc48217efabde0c4dca120ca103dadda7420d20
TaoLufula <lifulaj@gmail.com>
2024-01-07 03:31:20 -0500

    quick fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginPage from './pages/loginPage/Login'; 
-import RegisterPage from './pages/registerPage/Register'; 
-import HomePage from './pages/homePage/Home'; 
+import LoginPage from './pages/Login'; 
+import RegisterPage from './pages/Register'; 
+import HomePage from './pages/Home'; 
 
 function App() {
     return (
