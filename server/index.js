38502df51897b50d1fb7b7f7987dcafd8f7776b6
Yaw <yawasamoah13@hotmail.com>
2024-01-17 16:59:09 -0500

    Refactor registration, login to make use of cookies

--- server/index.js
+++ server/index.js
@@ -5,14 +5,26 @@ const chatRoutes = require("./routes/chat.js")
 const socketHandler = require("./socketHandler");
 const db_con = require("./connections.js");
 const http = require("http");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
+
+//Enable CORS to accept the jwt cookie for api calls
+const corsOptions = {
+    origin: "http://localhost:3000",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Access-Control-Allow-Credentials",
+    ],
+  };
 
 const app = express();
 
-
-
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
+app.use(cookieParser());
 app.use("/api/auth", authenticationRoutes);
 app.use("/api/chat", chatRoutes);
 
