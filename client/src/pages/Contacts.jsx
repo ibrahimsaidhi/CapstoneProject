7479af39aba49d35d4355adfdd16b729be8c874f
Yaw <yawasamoah13@hotmail.com>
2024-01-17 18:39:34 -0500

    Add some temp react code to test contacts

--- client/src/pages/Contacts.jsx
+++ client/src/pages/Contacts.jsx
@@ -1,9 +1,120 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from 'axios';
 
+
+//Page that will display the user contacts. Temp code added for now just to test contacts backend, will be replaced soon
 function Contacts() {
-  return (
-    <div>Contacts will be here</div>
-  )
-}
+    
+    const [data, setData] = useState(null);
+    const [searchResults, setSearchResults] = useState(null);
+    const [searchInput, setSearchInput] = useState("");
+    const [userAdd, setUserAdd] = useState("");
+    const [userRemove, setUserRemove] = useState("");
 
-export default Contacts
+    const api = axios.create({
+      baseURL: "http://localhost:5000/api",
+      withCredentials: true,
+    });
+
+    const handleChangeSearch = (e) => {
+      e.preventDefault();
+      setSearchInput(e.target.value);
+    };
+
+    const handleChangeAdd = (e) => {
+      e.preventDefault();
+      setUserAdd(e.target.value);
+    };
+
+    const handleChangeRemove = (e) => {
+      e.preventDefault();
+      setUserRemove(e.target.value);
+    };
+
+
+    function fetchData() {
+        api.get('/contacts/all')
+        .then((response) => {
+          console.log(response.data);
+          Object.keys(response.data.users).length !== 0 ? setData(JSON.stringify(response.data.users)) : setData("No contacts");
+        })
+        .catch((error) => {
+          console.log(error);
+          alert("Error. " +error.response.data.message);
+        });
+      }
+
+    React.useEffect(() => 
+    {
+      fetchData();
+    }, []);
+
+
+    function searchForUsers() {
+      api.get('/contacts/search/'+searchInput)
+      .then((response) => {
+        console.log(response.data);
+        Object.keys(response.data.users).length !== 0 ? setSearchResults(JSON.stringify(response.data.users)) : setSearchResults("No user with that name");
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Error. " +error.response.data.message);
+      });
+      }
+    
+    function addToContacts() {
+      const userData = {
+        friendId: userAdd,
+      };
+      api.post('/contacts/add', userData)
+      .then((response) => {
+        console.log(response.data);
+        Object.keys(response.data).length !== 0 ? fetchData() : console.log("Add didnt worked");
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Error. " +error.response.data.message);
+      });
+      }
+    async function removeFromContacts() {
+      const userData = {
+        friendId: userRemove,
+      };
+      api.post('/contacts/remove', userData)
+      .then((response) => {
+        console.log(response.data);
+        Object.keys(response.data).length !== 0 ? fetchData(): console.log("Remove didnt worked");
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Error. " +error.response.data.message);
+      });
+      }
+
+
+    return  <div className="App">
+              <h1>Contacts Page</h1>
+              <input
+              type="search"
+              placeholder="search for user"
+              onChange={handleChangeSearch}
+              value={searchInput} />
+              <button type="button" onClick={searchForUsers}>Search</button>
+              {!searchResults ? <p> No Search </p> : <div> <p> search results: </p> <ul>{searchResults}</ul> </div>} 
+              <input
+              type="search"
+              placeholder="add contact by id"
+              onChange={handleChangeAdd}
+              value={userAdd} />
+              <button type="button" onClick={addToContacts}>Add</button>
+              <input
+              type="search"
+              placeholder="remove contact by id"
+              onChange={handleChangeRemove}
+              value={userRemove} />
+              <button type="button" onClick={removeFromContacts}>Remove</button>
+              {!data ? <p> loading all contacts </p> : <div> <p> Contact: </p> <ul>{data}</ul> </div>} 
+            </div>;
+  };
+  
+  export default Contacts
