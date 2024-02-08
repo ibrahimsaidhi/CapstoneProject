62e8ae3b9171e24a070520c8591d8f60710ef71a
TaoLufula <lifulaj@gmail.com>
2024-02-08 14:54:07 -0500

    implemented #38

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import { NavLink, Outlet, useLocation } from 'react-router-dom';
-import { Navbar, NavbarBrand, NavbarItem, NavbarMenuToggle } from '@nextui-org/react';
+import { NavLink, Outlet, useNavigate } from 'react-router-dom';
+import { Navbar, NavbarBrand, NavbarItem } from '@nextui-org/react';
+import axios from 'axios';
 import '../styles/home.css';
 
 function Home() {
+
+  const navigate = useNavigate();
   
   const [fullName, setFullName] = useState('');
   useEffect (()=>{setFullName(sessionStorage.getItem("name"))});
+
+  const api = axios.create({
+    baseURL: "http://localhost:5000/api",
+    withCredentials: true,  
+  });
+
+const handleLogout = () => {
+  api.post('/auth/logout')
+    .then(response => {
+      alert(response.data.message); 
+      sessionStorage.removeItem("name");
+      setTimeout(() => navigate('/login'), 1000);
+    })
+    .catch(error => {
+      console.error('Logout failed:', error);
+    });
+};
+
 
   return (
     <>
@@ -19,6 +40,8 @@ function Home() {
           <NavLink to="/allchats" activeClassName="active">Chats</NavLink>
           &nbsp; &nbsp;
           <NavLink to="/contacts" activeClassName="active">Contacts</NavLink>
+          &nbsp; &nbsp;
+          <button onClick={handleLogout}>Logout</button>
         </NavbarItem>
       </Navbar>
       <br/>
