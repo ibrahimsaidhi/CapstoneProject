e9e1872c9771f9f5254f06e3ae6a25e41628c1b1
Yash Kapoor <yashkapoor1102@gmail.com>
2024-03-07 01:51:09 -0500

    Issue43 - Fix: ensuring the uploads directory gets created if it does not exist

--- server/controller/uploadController.js
+++ server/controller/uploadController.js
@@ -1,5 +1,6 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs').promises;
 
 /**
  * Configuring storage for multer
@@ -23,17 +24,38 @@ const upload = multer({ storage: storage }).single('file');
  * @param {Object} req - The request object received from the client
  * @param {Object} res - The response object that sends back data to the client
  */
-const uploadFile = (req, res) => {
-    upload(req, res, function(err) {
-        if (err) {
-            return res.status(500).json({ message: 'Error uploading file', error: err.message });
-        }
-        if (req.file) {
-            res.json({ filePath: `/uploads/${req.file.filename}` });
-        } else {
-            res.status(400).send('No file uploaded.');
-        }
-    });
+const uploadFile = async (req, res) => {
+    try {
+        // Checking if the "uploads" directory exists
+        await ensureUploadsDirExists();
+
+        upload(req, res, function(err) {
+            if (err) {
+                return res.status(500).json({ message: 'Error uploading file', error: err.message });
+            }
+            if (req.file) {
+                res.json({ filePath: `/uploads/${req.file.filename}` });
+            } else {
+                res.status(400).send('No file uploaded.');
+            }
+        });
+    } catch (error) {
+        return res.status(500).json({ message: "Error checking if the uploads directory exists"});
+    }
 };
+
+/**
+ * Checks if the "uploads" directory exists.
+ * If it does not exist, then it gets created.
+ */
+const ensureUploadsDirExists = async () => {
+    const uploadsDir = path.join(__dirname, '../uploads');
+    try {
+        await fs.access(uploadsDir);
+    } catch (error) {
+        console.log('Uploads directory does not exist, creating it.');
+        await fs.mkdir(uploadsDir, { recursive: true });
+    }
+}
 
 module.exports = { uploadFile };
