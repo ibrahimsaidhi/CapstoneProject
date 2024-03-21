6471644b07ca568685d8830d7290ad2595a1d745
Ibrahim Said <ibrahimsaid3@cmail.carleton.ca>
2024-03-20 20:46:55 -0400

    cookie fix 4

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -42,6 +42,7 @@ const login = (req, res) => {
       // Set the JWT as an HTTP-only cookie for added security
       res.cookie("accessToken", token, {
         httpOnly: true,
+        secure: true,
         SameSite: 'None',
         maxAge: cookieExp * 1000,
       });
