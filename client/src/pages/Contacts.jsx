ec321cb5276d295a23b408450577d9c96abc75e4
TaoLufula <lifulaj@gmail.com>
2024-03-22 05:15:49 -0400

    Styled contacts page

--- client/src/pages/Contacts.jsx
+++ client/src/pages/Contacts.jsx
@@ -169,7 +169,7 @@ function Contacts() {
 
     return (
       <div className="App">
-        <h1>Contacts Page</h1>
+        <h1>Your Contacts</h1>
         <div className="section">
           <input type="search" placeholder="search for user" onChange={handleChangeSearch} value={searchInput} />
           <button className="searchButton" type="button" onClick={searchForUsers}>Search</button>
