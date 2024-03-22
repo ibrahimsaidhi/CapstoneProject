77be05705aa37bf63863f1a684fa75a652681ba2
Yaw <yawasamoah13@hotmail.com>
2024-03-22 06:22:32 -0400

    Update chat list when new chat created, and fixes server from sending chat to inital none chat

--- client/src/pages/ChatBlock.jsx
+++ client/src/pages/ChatBlock.jsx
@@ -3,6 +3,8 @@
  * @author: Ibrahim Said
  */
 
+
+import { useState} from "react";
 import Chat from './Chat';
 import AllChats from './AllChats';
 import io from "socket.io-client";
@@ -16,13 +18,24 @@ const socket = io(`${process.env.REACT_APP_PARLONS_PROFILE_URL}`);
  * @returns a page that contains the chat-box next to the list of chats
  */
 const ChatBlock = () =>{
+
+    /**
+     * updateListCount reprsents prop that is passed down to all chat to let it now there has been a message sent to a new chat
+     */
+    const [updateListCount, setUpdateListCount] = useState(0);
+
+    function incrementUpdateListCount()
+    {
+        setUpdateListCount(() => updateListCount + 1);
+    }
+
     return (
         <div className='container'>
             <div className='chat-list'>
-                <AllChats/>
+                <AllChats listUpdate={updateListCount}/>
             </div>
             <div className='chat-box'>
-                <Chat socket={socket}/>
+                <Chat socket={socket} listUpdateFunc={incrementUpdateListCount}/>
             </div>
         </div>  
     );
